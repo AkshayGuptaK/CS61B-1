f87584a19be6a4e2e0ae62ffe6edc495a313ec3e
Kelley Liu <kelleyliu@berkeley.edu>
2019-03-05 15:55:26 -0800

    proj files

---
+++ galaxy/View.java
@@ -0,0 +1,15 @@
+package galaxy;
+
+/**
+ * A view of a Galaxies model.
+ *
+ * @author P. N. Hilfinger
+ */
+interface View {
+
+    /**
+     * Update the current view according to MODEL.
+     */
+    void update(Model model);
+
+}
